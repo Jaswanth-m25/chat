0cf7caf5d409842f56ba679f90717bc9d5202cb5
jaswanth-m25 <jaswanth6669@gmail.com>
2026-05-26 04:35:57 +0530

    Forced IPv4 for nodemailer

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -3,8 +3,9 @@ const nodemailer = require('nodemailer');
 // Configure transporter
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST || 'smtp.gmail.com',
-    port: process.env.EMAIL_PORT || 587,
+    port: process.env.EMAIL_PORT || 465,
     secure: process.env.EMAIL_SECURE === 'true',
+    family: 4,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
