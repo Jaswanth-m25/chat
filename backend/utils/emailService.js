48a229ddcdd449c9f9f6791f2c5b7e2d1e003182
jaswanth-m25 <jaswanth6669@gmail.com>
2026-05-26 05:03:51 +0530

    switched smtp to brevo

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -1,23 +1,16 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    host: '74.125.24.108',
-    port: 465,
-    secure: true,
+    host: process.env.EMAIL_HOST,
+    port: 587,
+    secure: false,
 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    },
-
-    tls: {
-        rejectUnauthorized: false
-    },
-
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000
+    }
 });
+
 transporter.verify((error, success) => {
     if (error) {
         console.log("SMTP ERROR:", error);
