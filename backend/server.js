eab518d2bf4cbad478c39f6540f6f0be808263c0
jaswanth-m25 <jaswanth6669@gmail.com>
2026-05-25 19:11:52 +0530

    added health route

--- backend/server.js
+++ backend/server.js
@@ -52,8 +52,12 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Internal server error' });
 });
+app.get('/', (req, res) => {
+  res.send('Chat backend running successfully');
+});
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
