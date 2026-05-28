504855ec7d6fadcb12077364f97ec64ec0d11a04
jaswanth-m25 <jaswanth6669@gmail.com>
2026-05-29 01:22:14 +0530

    fixed clear chat functionality bug new

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const messageController = require('../controllers/messageController');
+const authMiddleware = require('../middleware/auth');
+// const { clearChat } = require('../controllers/messageController');
 // const authenticateToken = require('../middleware/auth');
 
 // Get private messages with a user
@@ -17,5 +19,9 @@ router.put('/read/:messageId', messageController.markAsRead);
 
 // Upload a file/image
 router.post('/upload', messageController.upload.single('file'), messageController.uploadFile);
-
+router.delete(
+  '/clear/:userId',
+  authMiddleware,
+  messageController.clearChat
+);
 module.exports = router;
